e3000ae217bb5d8943e4735dbc831fe314e99d8e
Fuad <fmikayilov87@gmail.com>
2023-02-12 16:35:27 +0100

    first commit

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        private IEnumerable<object> weekends;
+
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekends)
+        {
+            if (dayCount < 0)
+            {
+                throw new ArgumentException("day count can not be less than 0", nameof(dayCount));
+            }
+
+            var res = startDate;
+            if (weekends is null)
+            {
+                res = startDate.AddDays(dayCount - 1);
+                return res;
+            }
+            var deadlineDate = startDate.AddDays(dayCount);
+            double nonWorkingDays;
+            foreach (var item in weekends)
+            {
+                if (deadlineDate > item.StartDate)
+                {
+                    nonWorkingDays = (item.EndDate - item.StartDate).TotalDays;
+                    res = startDate.AddDays(nonWorkingDays + dayCount);
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using CSharpTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDate = new DateTime(2021, 4, 21);
+            int count = -5;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2021, 4, 23), new DateTime(2021, 4, 23))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
